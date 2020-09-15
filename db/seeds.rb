aae8746a730d8da327e672a361033f3d2244bc2e
szyar <soezayarszyar@gmail.com>
2020-09-15 21:53:42 +0630

    create seed data to test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |i|
+  Post.create(title: "Note #{i + 1}", content: 'Lorem ipsum saves lives')
+end
